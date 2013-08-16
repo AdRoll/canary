7a7973f6ce90395021a72480a6057655ba51c56a
jesse-ad <jesse@adroll.com>
2013-08-16 11:47:38 -0700

    Now notifying metrics in a spawned function

--- src/shiv.erl
+++ src/shiv.erl
@@ -13,31 +13,30 @@
 -export([init/1, handle_call/3, handle_cast/2, terminate/2, code_change/3, handle_info/2]).
 
 %% api
--export([start/4, start/5, stop/0, ping/0, track_metric/1, notify_metric/2, send_new_relic_metrics/0]).
+-export([start/3, start/4, stop/0, ping/0, track_metric/1, notify_metric/2, send_new_relic_metrics/0]).
 
 -include("shiv.hrl").
 
 -record(server_state, {
     host_name,
     tracked_metrics,
-    new_relic_guid,
     new_relic_entity_name,
     new_relic_license,
     new_relic_report_pc   % TRef representing periodic call to upload metrics via new_relic_api
 }).
 
 
-start(NewRelicGuid, NewRelicEntityName, HostName, NewRelicLicense) ->
-    start(NewRelicGuid, NewRelicEntityName, HostName, NewRelicLicense, []).
+start(NewRelicEntityName, HostName, NewRelicLicense) ->
+    start(NewRelicEntityName, HostName, NewRelicLicense, []).
 
-start(NewRelicGuid, NewRelicEntityName, HostName, NewRelicLicense, InitialShivMetrics) ->
+start(NewRelicEntityName, HostName, NewRelicLicense, InitialShivMetrics) ->
     lager:info("Starting shiv..."),
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [NewRelicGuid, NewRelicEntityName, HostName, NewRelicLicense, InitialShivMetrics], []).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [NewRelicEntityName, HostName, NewRelicLicense, InitialShivMetrics], []).
 
 stop() ->
     gen_server:cast(?MODULE, stop).
 
-init([NewRelicGuid, NewRelicEntityName, HostName, NewRelicLicense, ShivMetrics]) ->
+init([NewRelicEntityName, HostName, NewRelicLicense, ShivMetrics]) ->
     process_flag(trap_exit, true),
     lager:info("Initializing shiv..."),
 
@@ -53,7 +52,6 @@ init([NewRelicGuid, NewRelicEntityName, HostName, NewRelicLicense, ShivMetrics])
     State = #server_state{
             host_name = HostName,
             tracked_metrics = ShivMetrics,
-            new_relic_guid = NewRelicGuid,
             new_relic_entity_name = NewRelicEntityName,
             new_relic_license = NewRelicLicense,
             new_relic_report_pc = NewRelicReportPC
@@ -106,8 +104,10 @@ track_metric(ShivMetric) ->
 %% @doc Notifies underlying folsom metric of some event.
 %%
 notify_metric(RelicMetricName, Value) ->
-    folsom_metrics:notify(
-        {to_folsom_name(RelicMetricName), Value}
+    erlang:spawn(
+        fun() ->
+            folsom_metrics:notify({to_folsom_name(RelicMetricName), Value})
+        end
     ).
 
 
@@ -187,15 +187,12 @@ handle_cast({track_metric, ShivMetric},
     {noreply, State#server_state{tracked_metrics = [ShivMetric | TrackedMetrics]}};
 handle_cast({send_new_relic_metrics, RelicMetrics},
     State = #server_state{
-            host_name = HostName,
-            new_relic_guid = NRGuid,
-            new_relic_license = NRLicense,
-            new_relic_entity_name = NREntityName
+            host_name = HostName, new_relic_license = NRLicense, new_relic_entity_name = NREntityName
     })
     ->
     erlang:spawn(
         fun() ->
-            new_relic_api:send_metrics(NRGuid, NREntityName, HostName, NRLicense, RelicMetrics)
+            new_relic_api:send_metrics(NREntityName, HostName, NRLicense, RelicMetrics)
         end
     ),
     {noreply, State};
